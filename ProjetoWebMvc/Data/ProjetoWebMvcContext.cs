d0b3f9bf68cc4b015db6686de760a9bca2614638
joyceguilhermina <joycesantos8124@gmail.com>
2021-12-03 12:42:59 -0300

    crud scaffolding

---
+++ ProjetoWebMvc/Data/ProjetoWebMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoWebMvc.Models;
+
+namespace ProjetoWebMvc.Data
+{
+    public class ProjetoWebMvcContext : DbContext
+    {
+        public ProjetoWebMvcContext (DbContextOptions<ProjetoWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjetoWebMvc.Models.Department> Department { get; set; }
+    }
+}
